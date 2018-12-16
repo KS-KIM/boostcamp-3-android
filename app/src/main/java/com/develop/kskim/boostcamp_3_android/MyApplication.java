87062bf4e7dfaac2b905313b5bb9ae505c18ca6a
KSKIM <kwangsoonkim@outlook.com>
2018-12-17 08:26:01 +0900

    컨텍스트 사용을 용이하게 하기 위한 어플리케이션 클래스 생성

---
+++ app/src/main/java/com/develop/kskim/boostcamp_3_android/MyApplication.java
@@ -0,0 +1,19 @@
+package com.develop.kskim.boostcamp_3_android;
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+
+    private static MyApplication mContext;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mContext = this;
+    }
+
+    public static MyApplication getContext() {
+        return mContext;
+    }
+
+}
